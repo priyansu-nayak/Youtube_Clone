34f7d19fae37931b146c43b941a4a503b725664e
priyansu-nayak <priyansu18.8@gmail.com>
2023-10-22 21:50:34 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
+
+import { Navbar, ChannelDetail, VideoDetail, SearchFeed, Feed } from './components'
+
 /*
-box is a simple div element
+box is a simple div element ,sx is its attribute for style
 
 */
+
+
 const App = () =>
 (
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }} >
-      Navbar
+      <Navbar />
       <Routes>
-        
+
         <Route path="/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
