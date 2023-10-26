b1d8903857f1282c177b6c0abf0a78bc7a285aa9
priyansu-nayak <priyansu18.8@gmail.com>
2023-10-26 12:18:27 +0530

    updated

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { categories } from '../utils/constants'
+
+
+const Sidebar = () => (
+
+    <Stack
+        direction="row"
+        sx={{
+            overflow: 'auto',
+            color: '#fff',
+            height: { sx: 'auto', md: '95%' },
+            flexDirection: { md: 'column' }
+        }}
+    >
+        {categories.map(category => (
+            <button>
+                <span>{category.icon}</span>
+                <span>{category.name}</span>
+            </button>   
+        ))}
+    </Stack >
+)
+
+export default Sidebar
