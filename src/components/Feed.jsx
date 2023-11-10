e4ea733bd5e3b646390d76a8500825f6e90f2fa5
priyansu-nayak <priyansu18.8@gmail.com>
2023-11-10 18:42:53 +0530

    updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -13,14 +13,19 @@ const Feed = () => {
 
   useEffect(()=>{
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-  },[]);
+  },[selectedCategory]);
 
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
 
-        <Sidebar />
+        <Sidebar
+         selectedCategory={selectedCategory}
+         setSelectedCategory={setSelectedCategory}
+        
+        />
+
         <Typography className="copyright" variant="body2"
           sx={{ mt: 1.5, color: '#fff' }}
         >
